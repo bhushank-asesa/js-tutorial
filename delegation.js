6bbc8f92c701348cea2c59c18511965d551a2ddd
Bhushank-asesa <bhushan.kumbhar@asesadigital.com>
2025-03-25 14:43:12 +0530

    event delegation

--- delegation.js
+++ delegation.js
@@ -1,6 +1,6 @@
 document.querySelector("#mainDiv").addEventListener("click", (e) => {
   // delegation saves the lots of memory, because there will large number of li may be dynamic if we add every li addEventListener then it will be memory consuming so delegation using bubbling saves memory
-  console.log("main div clicked", e.target, e.target.id, e.target.tagName);
+  console.log("main div clicked",e, e.target, e.target.id, e.target.tagName,e.target.dataset.value);
   if (e.target.tagName === "LI") {
     console.log("Li clicked");
   }
